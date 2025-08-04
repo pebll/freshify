ef3ddf0358492e8a6404d51b76e9ae21e22e909f
Badal Khiatani Ramos <149239859+baadeel@users.noreply.github.com>
2025-08-04 21:52:42 +0200

    Test scripts (#5)
    * First test homebrew install
    
    * Update README.md

---
+++ setup/lib/colors.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define colors using tput for better terminal compatibility
+PINK=$(tput setaf 205)
+PURPLE=$(tput setaf 141)
+GREEN=$(tput setaf 114)
+ORANGE=$(tput setaf 208)
+BLUE=$(tput setaf 75)
+YELLOW=$(tput setaf 221)
+RED=$(tput setaf 196)
+CYAN=$(tput setaf 51)
+WHITE=$(tput setaf 15)
+NC=$(tput sgr0) # No Color / Reset
